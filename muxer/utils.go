ab9d546b003ea184f0e021b36e2822696f2f4b6e
Cong Zuo <zckevinzc@gmail.com>
2023-07-04 22:20:26 +0800

    Fix: enable timestamp in sing logger

--- muxer/utils.go
+++ muxer/utils.go
@@ -1,13 +1,20 @@
 package muxer
 
-import "github.com/sagernet/sing-box/log"
+import (
+	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
+)
 
 var (
 	DebugMode = false
 )
 
 func NewLogger(name string) log.ContextLogger {
-	logFactory, err := log.New(log.Options{})
+	logFactory, err := log.New(log.Options{
+		Options: option.LogOptions{
+			Timestamp: true,
+		},
+	})
 	if err != nil {
 		panic(err)
 	}
